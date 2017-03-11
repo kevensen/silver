9a8e55d2b6b9b6a4e0268d782781de17648acd9e
Kenneth D. Evensen <kevensen@redhat.com>
2017-03-11 10:40:22 -0500

    Special image for java

---
+++ Dockerfile.java
@@ -0,0 +1,11 @@
+FROM silver/s2i-base-rhel7
+
+MAINTAINER Kenneth D. Evensen <kevensen@redhat.com>
+
+LABEL io.s2i.color=silver
+
+USER root
+RUN yum install java-1.8.0-openjdk-devel --disablerepo='*' --enablerepo='rhel-7-server-rpms' -y \
+    && yum clean all \ 
+    && rm -rf /var/cache/yum/*
+USER 1001
